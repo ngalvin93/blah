0bbbabefe19b49eeb24e363c9a2f7b5ef9d5125a
Alvin <32407008+ngalvin93@users.noreply.github.com>
2021-01-28 23:04:17 -0600

    Upload site

---
+++ script.js
@@ -0,0 +1,8 @@
+(function () {
+    document.addEventListener("DOMContentLoaded", function () {
+        var box = Array.from(document.getElementsByClassName('outer-box'));
+        box[0].style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        box[0].style.border = "5em solid #" + Math.floor(Math.random()*16777215).toString(16);
+    })
+})();
+
