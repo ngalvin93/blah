be8ac563c4cad02af572dc10fcc88ba7ea46808b
Alvin <32407008+ngalvin93@users.noreply.github.com>
2021-01-28 23:25:59 -0600

    Add click to change color

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 (function () {
+    var box = Array.from(document.getElementsByClassName("outer-box"));
     document.addEventListener("DOMContentLoaded", function () {
-        var box = Array.from(document.getElementsByClassName('outer-box'));
+        box[0].style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        box[0].style.border = "10vw solid #" + Math.floor(Math.random()*16777215).toString(16);
+    })
+    document.addEventListener("click", function () {
         box[0].style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
         box[0].style.border = "10vw solid #" + Math.floor(Math.random()*16777215).toString(16);
     })
 })();
-
